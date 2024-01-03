0322da5c6e117808b04737c86833635b2ce1fba7
Chidi Nwanneka <cnwanneka@gmail.com>
2024-01-03 07:46:57 +0000

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './components/Header';
+import Product from './components/Product';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Yearbe</h1>
+      <h2>We have some nice fitted sheets in stock.</h2>
     </div>
   );
 }
