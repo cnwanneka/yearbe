d1f4d1d862613d93f4dd201633170b499d7def9b
Chidi Nwanneka <cnwanneka@gmail.com>
2024-01-04 18:48:01 +0000

    added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,17 @@
+
+
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Navbar from './Navbar'; // Import Navbar component
 import './Header.css';
 
-const Header = () => {
-    return (
-        <header className="header">
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </ul>
-            </nav>
-        </header>
-    );
-};
+function Header() {
+  return (
+    <header>
+      <div className="logo">Yearbe</div>
+      <Navbar />
+      {/* You can add more elements here */}
+    </header>
+  );
+}
 
 export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
 
+import React from 'react';
+import './Home.css'; 
 import Product from './Product';
-// ...
 
 function Home() {
   return (
-    <div>
+    <div className="home-container"> 
       <h1>Welcome to Yearbe</h1>
       <Product title="White Fitted Double Bedsheet" price="9.99" description="High-quality double bedsheet" />
       <Product title="White Fitted Single Bedsheet" price="7.99" description="Comfortable single bedsheet" />
-      
     </div>
   );
 }
