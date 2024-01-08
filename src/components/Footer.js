360d1daaf01fbcb08bb57cad2e3cd30332e7b294
Chidi Nwanneka <cnwanneka@gmail.com>
2024-01-08 23:00:02 +0000

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p>&copy; {(new Date().getFullYear())} Yearbe. All rights reserved.</p>
-      <p>Contact us at: <a href="mailto:info@bedsheetstore.com">chidi@yearbe.com</a></p>
-      
+      <p>Contact us at: <a href="mailto:chidi@yearbe.com">chidi@yearbe.com</a></p>
     </footer>
   );
 }
