9fb3fdf89f030e8a3616b9bdd2e46ce1012b5158
Chidi Nwanneka <cnwanneka@gmail.com>
2024-01-08 11:59:28 +0000

    resized SingleBedsheetImage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './components/Home'; // Your Home component
-import About from './components/About'; // Your About component
-import Contact from './components/Contact'; // Your Contact component
-import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
+      <div className="app-container">
+        <Header />
+        <div className="content-wrap">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
