4014fae005706137b15332aabbc0450ef363c1d7
Chidi Nwanneka <cnwanneka@gmail.com>
2024-01-08 01:30:15 +0000

    added link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 
-
 import React from 'react';
-import Navbar from './Navbar'; // Import Navbar component
+import { Link } from 'react-router-dom'; // Import Link component
+import Navbar from './Navbar';
 import './Header.css';
 
 function Header() {
   return (
     <header>
-      <div className="logo">Yearbe</div>
+      <div className="logo">
+        {/* Add Link to wrap around "Yearbe" */}
+        <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+          Yearbe
+        </Link>
+      </div>
       <Navbar />
       {/* You can add more elements here */}
     </header>
