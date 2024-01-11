063c8b3418cecb5e47183cd267172e564c899eaa
Chidi Nwanneka <cnwanneka@gmail.com>
2024-01-11 10:44:05 +0000

    added cart items display

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,32 @@
 
-import React from 'react';
+// Navbar.js
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
 import { HiOutlineShoppingBag } from 'react-icons/hi';
+import { CartContext } from './CartContext'; // Import CartContext
+import './Navbar.css';
 
 function Navbar() {
-  
-  return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li className="shopping-cart-icon">
-          <Link to="/cart"><HiOutlineShoppingBag /></Link>
-        </li>    
-      </ul>
-    </nav>
-  );
+    const { cart } = useContext(CartContext);
+
+    // Calculate the total number of items in the cart
+    const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
+
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <li className="shopping-cart-icon">
+                    <Link to="/cart">
+                        <HiOutlineShoppingBag />
+                        {totalItems > 0 && <span className="cart-item-count">{totalItems}</span>}
+                    </Link>
+                </li>    
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
-
