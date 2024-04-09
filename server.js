c8f6baeec130052e785d9ff04181aff72e0ea336
Chidi Nwanneka <cnwanneka@gmail.com>
2024-04-09 18:29:40 +0100

    updated paymentpage

--- server.js
+++ server.js
@@ -9,42 +9,51 @@ const app = express();
 const port = 3001; // Can be any port that's free
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
-  methods: ['POST'], // Only allow POST requests from the specified origin
-  allowedHeaders: ['Content-Type'],
+    origin: 'http://localhost:3000',
+    methods: ['POST'], // Only allow POST requests from the specified origin
+    allowedHeaders: ['Content-Type'],
 };
 
 app.use(cors(corsOptions));
-
 app.use(bodyParser.json());
 
 // Route to handle payment
 app.post('/payment', async (req, res) => {
-  try {
-    const { amount, paymentMethodId } = req.body;
+    try {
+        const { amount, paymentMethodId, paymentIntentId } = req.body;
 
-    // Define your return URL. This should be a route in your app that can handle the next steps.
-    const return_url = 'http://localhost:3000/payment-success';
+        let paymentIntent;
+        if (paymentIntentId) {
+            // Attempt to retrieve an existing PaymentIntent
+            paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
-      currency: 'gbp',
-      payment_method: paymentMethodId,
-      confirmation_method: 'manual',
-      confirm: true,
-      // Include the return_url in your PaymentIntent creation
-      return_url: return_url,
-    });
+            // Check if the retrieved PaymentIntent has already succeeded
+            if (paymentIntent.status === 'succeeded') {
+                console.log(`PaymentIntent ${paymentIntent.id} has already succeeded.`);
+                return res.status(400).json({ error: "PaymentIntent already succeeded" });
+            }
+        }
 
-    res.json({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    console.error("Stripe error:", error); // Detailed log for debugging
-    res.status(500).json({ error: error.message });
-  }
+        // Proceed with creating a new PaymentIntent if none exists or if the existing one has not succeeded
+        if (!paymentIntent) {
+            paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'gbp',
+                payment_method: paymentMethodId,
+                confirmation_method: 'manual',
+                confirm: true,
+                // Include the return_url in your PaymentIntent creation
+                return_url: 'http://localhost:3000/payment-success',
+            });
+        }
+
+        res.json({ clientSecret: paymentIntent.client_secret });
+    } catch (error) {
+        console.error("Stripe error:", error); // Detailed log for debugging
+        res.status(500).json({ error: error.message });
+    }
 });
 
-
-
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
--- src/components/PaymentPage.js
+++ src/components/PaymentPage.js
@@ -9,7 +9,7 @@ function PaymentPage() {
     const navigate = useNavigate();
     const [deliveryDetails, setDeliveryDetails] = useState(null);
     const [amount, setAmount] = useState();
-    const [isProcessing, setIsProcessing] = useState(false); // Add this state to track if payment is processing
+    const [isProcessing, setIsProcessing] = useState(false); // State to track if payment is processing
     const stripe = useStripe();
     const elements = useElements();
 
@@ -26,69 +26,64 @@ function PaymentPage() {
     }, []);
 
     const handlePayment = async (e) => {
-        e.preventDefault();
-        if (!stripe || !elements || isProcessing) { // Check if already processing
-            return;
-        }
-
-        setIsProcessing(true); // Prevent further submissions
-
-        const cardElement = elements.getElement(CardNumberElement);
-        if (!cardElement) {
-            console.log('CardNumberElement not found');
-            setIsProcessing(false); // Reset processing state
-            return;
-        }
-
-        try {
-            // Create a payment method
-            const paymentMethodResponse = await stripe.createPaymentMethod({
-                type: 'card',
-                card: cardElement,
-            });
-
-            if (paymentMethodResponse.error) {
-                console.log(paymentMethodResponse.error.message);
-                alert('Payment failed: ' + paymentMethodResponse.error.message);
-                setIsProcessing(false); // Reset processing state
-                return;
-            }
-
-            // Now, send the payment method ID to your server
-            const response = await axios.post('http://localhost:3001/payment', {
-                paymentMethodId: paymentMethodResponse.paymentMethod.id,
-                amount: amount,
-            });
-
-            const { clientSecret } = response.data;
-
-            // Confirm the card payment
-            const result = await stripe.confirmCardPayment(clientSecret, {
-                payment_method: paymentMethodResponse.paymentMethod.id,
-            });
-
-            if (result.error) {
-                // Check for the specific Stripe error
-                if (result.error.code === "payment_intent_unexpected_state") {
-                    // Handle as a success or inform the user
-                    navigate('/confirmation');
-                } else {
-                    // Inform the customer that there was an error.
-                    console.error(result.error.message);
-                    alert("Payment error: " + result.error.message);
-                }
-            } else if (result.paymentIntent && result.paymentIntent.status === 'succeeded') {
-                // The payment has been processed!
-                alert('Payment successful');
-                handlePaymentSuccess(); // Handle the payment success
-            }
-        } catch (error) {
-            console.log(error);
-            alert('Payment failed');
-        } finally {
-            setIsProcessing(false); // Reset processing state regardless of outcome
-        }
+      e.preventDefault();
+      if (!stripe || !elements || isProcessing) {
+          console.log('Payment is already being processed or Stripe is not fully loaded.');
+          return;
+      }
+  
+      setIsProcessing(true);
+  
+      const cardElement = elements.getElement(CardNumberElement);
+      if (!cardElement) {
+          console.log('CardNumberElement not found');
+          setIsProcessing(false);
+          return;
+      }
+  
+      try {
+          const paymentMethodResponse = await stripe.createPaymentMethod({
+              type: 'card',
+              card: cardElement,
+          });
+  
+          if (paymentMethodResponse.error) {
+              console.log(paymentMethodResponse.error.message);
+              alert('Payment failed: ' + paymentMethodResponse.error.message);
+              return;
+          }
+  
+          const response = await axios.post('http://localhost:3001/payment', {
+              paymentMethodId: paymentMethodResponse.paymentMethod.id,
+              amount: amount,
+          });
+  
+          const { clientSecret } = response.data;
+  
+          const result = await stripe.confirmCardPayment(clientSecret, {
+              payment_method: paymentMethodResponse.paymentMethod.id,
+          });
+  
+          if (result.error) {
+              if (result.error.code === "payment_intent_unexpected_state") {
+                  console.log('Payment Intent has already been confirmed.');
+                  navigate('/confirmation');
+              } else {
+                  console.error(result.error.message);
+                  alert("Payment error: " + result.error.message);
+              }
+          } else if (result.paymentIntent && result.paymentIntent.status === 'succeeded') {
+              console.log('Payment successful');
+              handlePaymentSuccess();
+          }
+      } catch (error) {
+          console.error(error);
+          alert('Payment processing error');
+      } finally {
+          setIsProcessing(false);
+      }
     };
+  
 
     const handleChangeAddress = () => {
         localStorage.removeItem('deliveryDetails');
@@ -101,46 +96,46 @@ function PaymentPage() {
     };
 
     return (
-        <div className="payment-page-container">
-            <div className="billing-address">
-                <h2>1. Billing Address</h2>
-                <div className="address-container">
-                    {deliveryDetails ? (
-                        <>
-                            <div className="address-details">
-                                <pre>{deliveryDetails.address}</pre>
-                            </div>
-                            <button onClick={handleChangeAddress} className="change-button">Change</button>
-                        </>
-                    ) : (
-                        <p>Billing address not found.</p>
-                    )}
-                </div>
-            </div>
+      <div className="payment-page-container">
+          <div className="billing-address">
+              <h2>1. Billing Address</h2>
+              <div className="address-container">
+                  {deliveryDetails ? (
+                      <>
+                          <div className="address-details">
+                              <pre>{deliveryDetails.address}</pre>
+                          </div>
+                          <button onClick={handleChangeAddress} className="change-button">Change</button>
+                      </>
+                  ) : (
+                      <p>Billing address not found.</p>
+                  )}
+              </div>
+          </div>
 
-            <div className="payment-form">
-                <h2>2. Payment Details</h2>
-                <form onSubmit={handlePayment}>
-                    <div className="input-field">
-                        <label>Card Number</label>
-                        <CardNumberElement className="StripeElement" options={{ placeholder: "1234 5678 9012 3456" }} />
-                    </div>
-                    <div className="expiry-cvv-field">
-                        <div className="input-field half-width">
-                            <label>Expiry MM/YY</label>
-                            <CardExpiryElement className="StripeElement" options={{ placeholder: "MM/YY" }} />
-                        </div>
-                        <div className="input-field half-width">
-                            <label>CVV</label>
-                            <CardCvcElement className="StripeElement" options={{ placeholder: "3 digits" }} />
-                        </div>
-                    </div>
-                    <button type="submit" disabled={isProcessing}>
-                        {isProcessing ? 'Processing…' : `Pay £${(amount / 100).toFixed(2)}`}
-                    </button>
-                </form>
-            </div>
-        </div>
+          <div className="payment-form">
+              <h2>2. Payment Details</h2>
+              <form onSubmit={handlePayment}>
+                  <div className="input-field">
+                      <label>Card Number</label>
+                      <CardNumberElement className="StripeElement" options={{ placeholder: "1234 5678 9012 3456" }} />
+                  </div>
+                  <div className="expiry-cvv-field">
+                      <div className="input-field half-width">
+                          <label>Expiry MM/YY</label>
+                          <CardExpiryElement className="StripeElement" options={{ placeholder: "MM/YY" }} />
+                      </div>
+                      <div className="input-field half-width">
+                          <label>CVV</label>
+                          <CardCvcElement className="StripeElement" options={{ placeholder: "3 digits" }} />
+                      </div>
+                  </div>
+                  <button type="submit" disabled={isProcessing}>
+                      {isProcessing ? 'Processing…' : `Pay £${(amount / 100).toFixed(2)}`}
+                  </button>
+              </form>
+          </div>
+      </div>
     );
 }
 
